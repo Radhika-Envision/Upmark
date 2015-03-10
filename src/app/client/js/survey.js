33de11f2830970ed97970caf6c81afbb918eac23
Alex Fraser <adfries@vpac.org>
2015-03-10 15:41:11 +1100

    Rendering responses based on JSON schema.

--- src/app/client/js/survey.js
+++ src/app/client/js/survey.js
@@ -1,11 +1,49 @@
 'use strict';
 
-angular.module('wsaa.survey', [])
+angular.module('wsaa.survey', ['ngResource'])
 
 
-.controller('SurveyCtrl', ['$scope',
-        function($scope) {
+.factory('Schema', ['$resource', function($resource) {
+    return $resource('/schema/:name.json', {name: '@name'}, {
+        get: { method: 'GET' }
+    });
+}])
+
+
+.controller('SurveyCtrl', ['$scope', 'Schema',
+        function($scope, Schema) {
+
+    $scope.measure = {
+        description: [
+            {
+                name: "Intent",
+                text: "To identify the extent to which the agency has a Board/Executive team approved policy in place for capital program prioritisation. The policy should be consistent with other agency policies, refers to the key outcomes or objectives to be achieved through capital program prioritisation, outline the approach to prioritisation, and provide sufficient flexibility in unusual circumstances."
+            }
+        ],
+        responseType: 'standard'
+    };
+    $scope.schema = null;
+
+    $scope.$watch('measure.responseType', function(responseType) {
+        $scope.schema = Schema.get({name: responseType});
+    });
 
 }])
 
+
+.controller('ResponseStandardCtrl', ['$scope', ResponseStandardCtrl])
+
 ;
+
+
+function BaseResponseCtrl($scope) {
+};
+
+function ResponseStandardCtrl($scope) {
+    BaseResponseCtrl.call(this, $scope);
+
+    $scope.$watch('schema', function(schema) {
+        $scope.activeResponse = schema.responses[0];
+    });
+};
+ResponseStandardCtrl.prototype = Object.create(BaseResponseCtrl.prototype)
