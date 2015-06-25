bcbb75140163b60a71eac7f8db970959bb7208f9
Alex Fraser <adfries@vpac.org>
2015-06-25 11:04:52 +1000

    Setting page title.
    Added a directive that sets the page title. To use it, add it to
    an element that contains text that should be included in the title.

--- src/app/client/js/widgets.js
+++ src/app/client/js/widgets.js
@@ -147,4 +147,41 @@ angular.module('vpac.widgets', [])
     };
 }])
 
+
+.directive('pageTitle', ['$document', '$injector', function($document, $injector) {
+    var numTitles = 0;
+    var defaultTitle = $document[0].title;
+    return {
+        restrict: 'EA',
+        link: function(scope, elem, attrs) {
+            if (numTitles > 0) {
+                // Don't install if there is already a title active (for nested
+                // pages)
+                return;
+            }
+
+            var prefix = '', suffix = '';
+            if ($injector.has('pageTitlePrefix'))
+                prefix = $injector.get('pageTitlePrefix');
+            if ($injector.has('pageTitleSuffix'))
+                suffix = $injector.get('pageTitleSuffix');
+
+            scope.$watch(
+                function() {
+                    return elem.text();
+                },
+                function(title) {
+                    $document[0].title = prefix + title + suffix;
+                }
+            );
+            numTitles++;
+
+            scope.$on('$destroy', function() {
+                numTitles--;
+                $document[0].title = defaultTitle;
+            });
+        }
+    };
+}])
+
 ;
