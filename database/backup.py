0b6303dcae096f1aa808c45b0ce8f1f3eade8219
Jin Park <forjin@vpac-innovations.com.au>
2015-07-17 09:30:39 +1000

    add backup script for aws

---
+++ database/backup.py
@@ -0,0 +1,34 @@
+import boto3
+import datetime
+
+client = boto3.client('rds')
+
+response = client.describe_db_snapshots(
+    DBInstanceIdentifier='postgres',
+)
+
+now = datetime.datetime.now().replace(tzinfo=None)
+
+response = [r for r in response["DBSnapshots"] if r[
+    "DBSnapshotIdentifier"].lower().startswith("weekly")]
+
+if response:
+    last_snapshot = sorted(
+        response, key=lambda snapshot: snapshot['InstanceCreateTime'], reverse=False)[0]
+
+    if last_snapshot:
+        week_ago = now + datetime.timedelta(days=-7)
+        print(week_ago)
+        print(last_snapshot['InstanceCreateTime'])
+        if last_snapshot['InstanceCreateTime'].replace(tzinfo=None) < week_ago:
+            create_weekly_backup()
+else:
+    create_weekly_backup()
+
+
+def create_weekly_backup():
+    client.create_db_snapshot(
+        DBSnapshotIdentifier='WeeklyBackup-{0}-{1}-{2}'.format(
+            now.day, now.month, now.year),
+        DBInstanceIdentifier='postgres',
+    )
