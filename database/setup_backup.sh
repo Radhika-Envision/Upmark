c7a21f9345e7592a69b1a6d0ecc9f530590ef8f9
Jin Park <forjin@vpac-innovations.com.au>
2015-07-22 12:42:14 +1000

    setup script added.

--- database/setup_backup.sh
+++ database/setup_backup.sh
@@ -13,12 +13,11 @@ function install_deps() {
 
     sudo apt-get update && apt-get install -y python3-pip
     pip3 install -vU boto3 awscli
-    pip3 install -r ${projdir}/requirements.txt
     if [ $? -ne 0 ]; then
         error "Failed to install Python dependencies."
         exit 1
     fi
-
+    aws configure    
 
 }
 
