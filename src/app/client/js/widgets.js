cdb2d64b126a593b5f3f3f538546b537919af5d6
Alex Fraser <adfries@vpac.org>
2015-09-09 11:17:39 +1000

    Animating height change for submission list
    This makes it easier for the eye to track what is changing.

--- src/app/client/js/widgets.js
+++ src/app/client/js/widgets.js
@@ -402,7 +402,8 @@ angular.module('vpac.widgets', [])
 .directive('autoresize', [function() {
     return {
         restrict: 'AC',
-        link: function(scope, elem, attrs) {
+        require: '?ngModel',
+        link: function(scope, elem, attrs, ngModel) {
             var resize = function() {
                 // Resize to something small first in case we should shrink -
                 // otherwise scrollHeight will be wrong.
@@ -413,11 +414,46 @@ angular.module('vpac.widgets', [])
             };
 
             elem.on('input change', resize);
-            scope.$watch(attrs.ngModel, resize);
+            ngModel.$viewChangeListeners.push(resize);
 
             scope.$on('$destroy', function() {
                 elem.off();
                 elem = null;
+            });
+        }
+    };
+}])
+
+
+/**
+ * Takes its height from a child element. This allows CSS transitions to be used
+ * for the natural height of the element.
+ */
+.directive('surrogateHeight', [function() {
+    return {
+        restrict: 'AC',
+        controller: [function() {}],
+        require: 'surrogateHeight',
+        link: function(scope, elem, attrs, surrogateHeight) {
+            surrogateHeight.update = function(height) {
+                elem.height(height);
+            };
+            if (attrs.surrogateHeight != '')
+                elem.height(Number(attrs.surrogateHeight));
+        }
+    };
+}])
+
+
+.directive('surrogateHeightTarget', [function() {
+    return {
+        restrict: 'AC',
+        require: '^surrogateHeight',
+        link: function(scope, elem, attrs, surrogateHeight) {
+            scope.$watch(function() {
+                return elem[0].scrollHeight;
+            }, function(height) {
+                surrogateHeight.update(height);
             });
         }
     };
