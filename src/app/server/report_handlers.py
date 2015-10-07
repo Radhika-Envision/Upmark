a197abf61fc8f53060f0c1c5f968bc914a512bbd
Alex Fraser <adfries@vpac.org>
2015-10-07 14:43:49 +1100

    Refactored diff handler
    To construct qnode and measure queries in separate functions. The
    code was becoming hard to follow.
    
    Also the queries now just return the entity IDs - this is the first
    step to a recursive query.

--- src/app/server/report_handlers.py
+++ src/app/server/report_handlers.py
@@ -34,122 +34,17 @@ class DiffHandler(handlers.Paginate, handlers.BaseHandler):
             raise handlers.ModelError("Hierarchy ID required")
 
         with model.session_scope() as session:
-            QA = model.QuestionNode
-            QB = aliased(model.QuestionNode, name='qnode_b')
-            MA = model.Measure
-            MB = aliased(model.Measure, name='measure_b')
-            QMA = model.QnodeMeasure
-            QMB = aliased(model.QnodeMeasure, name='qnode_measure_link_b')
-
-            # Find modified / relocated qnodes
-            qnode_query = (session.query(QA, QB)
-                .join(QB, QA.id == QB.id)
-
-                # Basic survey membership
-                .filter(QA.survey_id == survey_id_a,
-                        QB.survey_id == survey_id_b,
-                        QA.hierarchy_id == hierarchy_id,
-                        QB.hierarchy_id == hierarchy_id)
-
-                # Filter for modified objects
-                .filter((QA.title != QB.title) |
-                        (QA.description != QB.description) |
-                        (QA.parent_id != QB.parent_id) |
-                        (QA.seq != QB.seq))
-            )
-
-            # Find deleted qnodes
-            qnode_del_query = (session.query(QA, literal(None))
-                .select_from(QA)
-                .filter(QA.survey_id == survey_id_a,
-                        QA.hierarchy_id == hierarchy_id,
-                        ~QA.id.in_(
-                            session.query(QB.id)
-                                .filter(QB.survey_id == survey_id_b,
-                                        QB.hierarchy_id == hierarchy_id)))
-            )
-
-            # Find added qnodes
-            qnode_add_query = (session.query(literal(None), QB)
-                .select_from(QB)
-                .filter(QB.survey_id == survey_id_b,
-                        QB.hierarchy_id == hierarchy_id,
-                        ~QB.id.in_(
-                            session.query(QA.id)
-                                .filter(QA.survey_id == survey_id_a,
-                                        QA.hierarchy_id == hierarchy_id)))
-            )
-
-            # Find modified / relocated measures
-            qnode_measure_query = (session.query(MA, MB)
-
-                .join(MB, MA.id == MB.id)
-
-                .join(QMA, (QMA.survey_id == MA.survey_id) & (QMA.measure_id == MA.id))
-                .join(QMB, (QMB.survey_id == MB.survey_id) & (QMB.measure_id == MB.id))
-                .join(QA, (QMA.survey_id == QA.survey_id) & (QMA.qnode_id == QA.id))
-                .join(QB, (QMB.survey_id == QB.survey_id) & (QMB.qnode_id == QB.id))
-
-                # Basic survey membership
-                .filter(QA.survey_id == survey_id_a,
-                        QB.survey_id == survey_id_b,
-                        QA.hierarchy_id == hierarchy_id,
-                        QB.hierarchy_id == hierarchy_id)
-
-                # Filter for modified objects
-                .filter((MA.title != MB.title) |
-                        (MA.intent != MB.intent) |
-                        (MA.inputs != MB.inputs) |
-                        (MA.scenario != MB.scenario) |
-                        (MA.questions != MB.questions) |
-                        (MA.response_type != MB.response_type) |
-                        (MA.weight != MB.weight) |
-                        (QMA.qnode_id != QMB.qnode_id) |
-                        (QMA.seq != QMB.seq))
-            )
-
-            # Find deleted measures
-            qnode_measure_del_query = (session.query(MA, literal(None))
-                .select_from(MA)
-                .join(QMA, (QMA.survey_id == MA.survey_id) & (QMA.measure_id == MA.id))
-                .join(QA, (QMA.survey_id == QA.survey_id) & (QMA.qnode_id == QA.id))
-                .filter(QA.survey_id == survey_id_a,
-                        QA.hierarchy_id == hierarchy_id,
-                        ~QMA.measure_id.in_(
-                            session.query(QMB.measure_id)
-                                .join(QB, (QMB.survey_id == QB.survey_id) & (QMB.qnode_id == QB.id))
-                                .filter(QB.survey_id == survey_id_b,
-                                        QB.hierarchy_id == hierarchy_id)))
-            )
-
-            # Find added measures
-            qnode_measure_add_query = (session.query(literal(None), MB)
-                .select_from(MB)
-                .join(QMB, (QMB.survey_id == MB.survey_id) & (QMB.measure_id == MB.id))
-                .join(QB, (QMB.survey_id == QB.survey_id) & (QMB.qnode_id == QB.id))
-                .filter(QB.survey_id == survey_id_b,
-                        QB.hierarchy_id == hierarchy_id,
-                        ~QMB.measure_id.in_(
-                            session.query(QMA.measure_id)
-                                .join(QA, (QMA.survey_id == QA.survey_id) & (QMA.qnode_id == QA.id))
-                                .filter(QA.survey_id == survey_id_a,
-                                        QA.hierarchy_id == hierarchy_id)))
-            )
-
-            #log.error('Query: %s', qnode_query)
-            #log.error('Query: %s', qnode_del_query)
-            #log.error('Query: %s', qnode_measure_query)
+            qnode_query = self.construct_qnode_query(
+                session, survey_id_a, survey_id_b, hierarchy_id)
+            measure_query = self.construct_measure_query(
+                session, survey_id_a, survey_id_b, hierarchy_id)
 
             qnode_query = self.paginate(qnode_query)
-            qnode_measure_query = self.paginate(qnode_measure_query)
+            measure_query = self.paginate(measure_query)
             import pprint
             log.error('%s', pprint.pformat(
                 qnode_query.all()
-                + qnode_del_query.all()
-                + qnode_add_query.all()
-                + qnode_measure_query.all()
-                + qnode_measure_del_query.all()
-                + qnode_measure_add_query.all()
+                + measure_query.all()
             ))
             to_son = ToSon(include=[
                 r'/id$',
@@ -168,13 +63,128 @@ class DiffHandler(handlers.Paginate, handlers.BaseHandler):
             son = {}
             son['diff'] = to_son(
                 qnode_query.all()
-                + qnode_del_query.all()
-                + qnode_add_query.all()
-                + qnode_measure_query.all()
-                + qnode_measure_del_query.all()
-                + qnode_measure_add_query.all()
+                + measure_query.all()
             )
 
         self.set_header("Content-Type", "application/json")
         self.write(json_encode(son))
         self.finish()
+
+    def construct_qnode_query(
+            self, session, survey_id_a, survey_id_b, hierarchy_id):
+        QA = model.QuestionNode
+        QB = aliased(model.QuestionNode, name='qnode_b')
+
+        # Find modified / relocated qnodes
+        qnode_mod_query = (session.query(QA.id, QB.id)
+            .join(QB, QA.id == QB.id)
+
+            # Basic survey membership
+            .filter(QA.survey_id == survey_id_a,
+                    QB.survey_id == survey_id_b,
+                    QA.hierarchy_id == hierarchy_id,
+                    QB.hierarchy_id == hierarchy_id)
+
+            # Filter for modified objects
+            .filter((QA.title != QB.title) |
+                    (QA.description != QB.description) |
+                    (QA.parent_id != QB.parent_id) |
+                    (QA.seq != QB.seq))
+        )
+
+        # Find deleted qnodes
+        qnode_del_query = (session.query(QA.id, literal(None))
+            .select_from(QA)
+            .filter(QA.survey_id == survey_id_a,
+                    QA.hierarchy_id == hierarchy_id,
+                    ~QA.id.in_(
+                        session.query(QB.id)
+                            .filter(QB.survey_id == survey_id_b,
+                                    QB.hierarchy_id == hierarchy_id)))
+        )
+
+        # Find added qnodes
+        qnode_add_query = (session.query(literal(None), QB.id)
+            .select_from(QB)
+            .filter(QB.survey_id == survey_id_b,
+                    QB.hierarchy_id == hierarchy_id,
+                    ~QB.id.in_(
+                        session.query(QA.id)
+                            .filter(QA.survey_id == survey_id_a,
+                                    QA.hierarchy_id == hierarchy_id)))
+        )
+
+        qnode_query = (qnode_mod_query
+                       .union_all(qnode_add_query)
+                       .union_all(qnode_del_query))
+        return qnode_query
+
+    def construct_measure_query(
+            self, session, survey_id_a, survey_id_b, hierarchy_id):
+        QA = model.QuestionNode
+        QB = aliased(model.QuestionNode, name='qnode_b')
+        MA = model.Measure
+        MB = aliased(model.Measure, name='measure_b')
+        QMA = model.QnodeMeasure
+        QMB = aliased(model.QnodeMeasure, name='qnode_measure_link_b')
+
+        # Find modified / relocated measures
+        measure_mod_query = (session.query(MA.id, MB.id)
+
+            .join(MB, MA.id == MB.id)
+
+            .join(QMA, (QMA.survey_id == MA.survey_id) & (QMA.measure_id == MA.id))
+            .join(QMB, (QMB.survey_id == MB.survey_id) & (QMB.measure_id == MB.id))
+            .join(QA, (QMA.survey_id == QA.survey_id) & (QMA.qnode_id == QA.id))
+            .join(QB, (QMB.survey_id == QB.survey_id) & (QMB.qnode_id == QB.id))
+
+            # Basic survey membership
+            .filter(QA.survey_id == survey_id_a,
+                    QB.survey_id == survey_id_b,
+                    QA.hierarchy_id == hierarchy_id,
+                    QB.hierarchy_id == hierarchy_id)
+
+            # Filter for modified objects
+            .filter((MA.title != MB.title) |
+                    (MA.intent != MB.intent) |
+                    (MA.inputs != MB.inputs) |
+                    (MA.scenario != MB.scenario) |
+                    (MA.questions != MB.questions) |
+                    (MA.response_type != MB.response_type) |
+                    (MA.weight != MB.weight) |
+                    (QMA.qnode_id != QMB.qnode_id) |
+                    (QMA.seq != QMB.seq))
+        )
+
+        # Find deleted measures
+        measure_del_query = (session.query(MA.id, literal(None))
+            .select_from(MA)
+            .join(QMA, (QMA.survey_id == MA.survey_id) & (QMA.measure_id == MA.id))
+            .join(QA, (QMA.survey_id == QA.survey_id) & (QMA.qnode_id == QA.id))
+            .filter(QA.survey_id == survey_id_a,
+                    QA.hierarchy_id == hierarchy_id,
+                    ~QMA.measure_id.in_(
+                        session.query(QMB.measure_id)
+                            .join(QB, (QMB.survey_id == QB.survey_id) & (QMB.qnode_id == QB.id))
+                            .filter(QB.survey_id == survey_id_b,
+                                    QB.hierarchy_id == hierarchy_id)))
+        )
+
+        # Find added measures
+        measure_add_query = (session.query(literal(None), MB.id)
+            .select_from(MB)
+            .join(QMB, (QMB.survey_id == MB.survey_id) & (QMB.measure_id == MB.id))
+            .join(QB, (QMB.survey_id == QB.survey_id) & (QMB.qnode_id == QB.id))
+            .filter(QB.survey_id == survey_id_b,
+                    QB.hierarchy_id == hierarchy_id,
+                    ~QMB.measure_id.in_(
+                        session.query(QMA.measure_id)
+                            .join(QA, (QMA.survey_id == QA.survey_id) & (QMA.qnode_id == QA.id))
+                            .filter(QA.survey_id == survey_id_a,
+                                    QA.hierarchy_id == hierarchy_id)))
+        )
+
+        measure_query = (measure_mod_query
+                         .union_all(measure_add_query)
+                         .union_all(measure_del_query))
+        return measure_query
