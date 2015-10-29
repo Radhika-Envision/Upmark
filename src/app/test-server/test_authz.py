34263877f168e87dfa8e770f52db25842f12bb92
Jin <forjin@192-168-1-11.tpgi.com.au>
2015-10-29 21:56:05 +1100

    add unittest for statistics. but not finalize yet.

---
+++ src/app/test-server/test_authz.py
@@ -0,0 +1,122 @@
+import logging
+import os
+import pprint
+import unittest
+from unittest import mock
+import urllib
+
+import sqlalchemy
+from sqlalchemy.sql import func
+from sqlalchemy.orm.session import make_transient
+from tornado.escape import json_encode
+from tornado.testing import AsyncHTTPTestCase
+from tornado.web import Application
+
+import app
+import base
+import model
+from utils import ToSon
+
+
+log = logging.getLogger('app.test_authz')
+
+
+class StatisticsAuthzTest(base.AqHttpTestBase):
+
+    ### There is no meaning for testing authz for statistics.
+    ### Because user only request for whole survey statistics.
+    ### and for assessment, user alredy checked assessment authz
+    ### therefore only testing for the survey is purchased or not for
+    ### the organization is important.
+    def test_get_statistics(self):
+        with model.session_scope() as session:
+            survey = session.query(model.Survey).one()
+            organisation = (session.query(model.Organisation)
+                    .filter_by(name='Utility')
+                    .one())
+            hierarchy = (session.query(model.Hierarchy)
+                    .filter_by(title='Hierarchy 1')
+                    .one())
+
+            survey_id = str(survey.id)
+            organisation_id = str(organisation.id)
+            hierarchy_id = str(hierarchy.id)
+
+        with base.mock_user('admin'):
+            ## purchase the survey
+            self.fetch(
+                "/organisation/%s/hierarchy/%s.json?surveyId=%s" %
+                (organisation_id, hierarchy_id, survey_id),
+                method='PUT', body='', expected=200)
+
+            assessment_son = {'title': "Assessment"}
+            assessment_son = self.fetch(
+                "/assessment.json?orgId=%s&surveyId=%s&hierarchyId=%s" %
+                (organisation_id, survey_id, hierarchy_id),
+                method='POST', body=json_encode(assessment_son),
+                expected=200, decode=False)
+
+class ExporterAuthzTest(base.AqHttpTestBase):
+    def setUp(self):
+        super().setUp()
+        with model.session_scope() as session:
+            survey = session.query(model.Survey).one()
+            organisation = (session.query(model.Organisation)
+                    .filter_by(name='Utility')
+                    .one())
+            hierarchy = (session.query(model.Hierarchy)
+                    .filter_by(title='Hierarchy 1')
+                    .one())
+
+            self.survey_id = str(survey.id)
+            self.organisation_id = str(organisation.id)
+            self.hierarchy_id = str(hierarchy.id)        
+
+    def test_structure_exporter(self):
+        with base.mock_user('author'):
+            self.fetch("/export/survey/%s/hierarchy/%s.xlsx" % 
+                (self.survey_id, self.hierarchy_id),
+                method='GET', expected=200)
+
+    def test_assessment_exporter(self):
+        with base.mock_user('admin'):
+            self.purchase_survey()
+            self.add_assessment()
+        with model.session_scope() as session:
+            assessment = session.query(model.Assessment).filter(
+                            model.Assessment.survey_id==self.survey_id,
+                            model.Assessment.organisation_id==self.organisation_id,
+                            model.Assessment.hierarchy_id==self.hierarchy_id).one()
+        with base.mock_user('author'):
+            self.fetch(
+                "/export/assessment/%s.xlsx" % assessment.id,
+                method='GET', expected=200, decode=False)
+
+
+    def test_response_exporter(self):
+        with base.mock_user('admin'):
+            self.purchase_survey()
+            self.add_assessment()
+        with model.session_scope() as session:
+            assessment = session.query(model.Assessment).filter(
+                            model.Assessment.survey_id==self.survey_id,
+                            model.Assessment.organisation_id==self.organisation_id,
+                            model.Assessment.hierarchy_id==self.hierarchy_id).one()
+        with base.mock_user('author'):
+            self.fetch(
+                "/export/response/%s.xlsx" % assessment.id,
+                method='GET', expected=200, decode=False)
+
+    def purchase_survey(self):
+        self.fetch(
+            "/organisation/%s/hierarchy/%s.json?surveyId=%s" %
+            (self.organisation_id, self.hierarchy_id, self.survey_id),
+            method='PUT', body='', expected=200)
+
+    def add_assessment(self):
+        assessment_son = {'title': "Assessment"}
+        assessment_son = self.fetch(
+            "/assessment.json?orgId=%s&surveyId=%s&hierarchyId=%s" %
+            (self.organisation_id, self.survey_id, self.hierarchy_id),
+            method='PUT', body=json_encode(assessment_son),
+            expected=200, decode=False)
