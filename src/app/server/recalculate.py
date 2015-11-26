f255a1fff64ea154aa7b5221b5d69bd6b94633fb
Jin Park <forjin@vpac-innovations.com.au>
2015-11-26 14:15:58 +1100

    1. add modified field for recalculating. 2. recalculate process for docker

---
+++ src/app/server/recalculate.py
@@ -0,0 +1,30 @@
+import os
+import logging
+import time
+import model
+import app
+
+
+log = logging.getLogger('recalculate')
+time.sleep(10)
+
+
+app.connect_db()
+
+
+while True:
+    with model.session_scope() as session:
+        sub = (session.query(model.Assessment)
+            .join(model.Hierarchy)
+            .filter(model.Hierarchy.modified > model.Assessment.modified)
+            .first())
+        if sub is not None:
+            log.info("sub: %s", sub)
+            print("sub: %s", sub)
+            sub.update_stats_descendants()
+            sub.modified = sub.hierarchy.modified
+            session.commit()
+
+    log.info("Heart beat!!")
+    print("Heart beat!!")
+    time.sleep(10)
