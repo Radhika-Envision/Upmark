fb46bbd1db99c6bb285fc9f15d44c948d4770fa9
Jin Park <forjin@vpac-innovations.com.au>
2015-11-26 15:02:40 +1100

    logging enabled for recalculate.

--- src/app/server/recalculate.py
+++ src/app/server/recalculate.py
@@ -1,17 +1,19 @@
 import logging
 import time
 import model
-import app
+import datetime
 
+from app import connect_db
+
+connect_db()
 
 log = logging.getLogger('recalculate')
+log.setLevel(logging.INFO)
+
 interval = 300
+log.info("Starting service...:%s", datetime.datetime.utcnow())
+
 time.sleep(interval)
-
-
-app.connect_db()
-
-
 while True:
     with model.session_scope() as session:
         sub = (session.query(model.Assessment)
@@ -20,11 +22,9 @@ while True:
             .first())
         if sub is not None:
             log.info("sub: %s", sub)
-            print("sub: %s", sub)
             sub.update_stats_descendants()
             sub.modified = sub.hierarchy.modified
             session.commit()
 
-    log.info("Heart beat!!")
-    print("Heart beat!!")
+    log.info("Job finished: %s", datetime.datetime.utcnow())
     time.sleep(interval)
