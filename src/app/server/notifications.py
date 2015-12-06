c03f58fe293e9c4a6aed48dd8a8c1285ddb6e268
Jin Park <forjin@vpac-innovations.com.au>
2015-12-07 10:44:31 +1100

    1. change notification message format 2. notification logic changed

--- src/app/server/notifications.py
+++ src/app/server/notifications.py
@@ -1,31 +1,61 @@
-import logging
-import time
-import model
 import datetime
-
-from mail import send, get_config
-from string import Template
 from email.mime.text import MIMEText
-from app import connect_db
+import logging
+import os
+from string import Template
+import time
 
-log = logging.getLogger('app.notifications')
+import model
+import activity
+from mail import send, get_config
+
+logging.basicConfig(format='%(asctime)s %(message)s')
+log = logging.getLogger('notifications')
 log.setLevel(logging.INFO)
 
 interval = 10 # seconds
 
-def mail_content(user):
-    return user.email
 
-def send_email(config, user):
+def mail_content(activities):
+    content = ''
+    # content += "Title: %s\n" % err['submission_title']
+    # content += "Message: %s\n\n" % err['error']
+    for act in activities:
+        if act.verbs != 'broadcast':
+            content += act.message + ' '
+            content += act.subject.name + ' '
+            content += 'this ' + act.ob_type + ' - '
+            content += str(act.created) + '\n'
+        else:
+            content += act.message + '\n'
+    return content
+
+
+def send_email(config, user, activities):
 
     template = Template(config['MESSAGE_CONTENT'])
-    msg = MIMEText(template.substitute(message=mail_content(user)), 'text/plain')
+    msg = MIMEText(template.substitute(message=mail_content(activities)), 'text/plain')
 
     msg['Subject'] = config['MESSAGE_SUBJECT']
     msg['From'] = config['MESSAGE_SEND_FROM']
     msg['To'] = user.email
 
     send(config, msg)
+
+
+def get_activities(session, user):
+
+    now = datetime.datetime.utcnow()
+    activities = activity.Activities(session)
+    from_date = user.email_time
+    if from_date == None:
+        from_date = now + datetime.timedelta(days=-7)
+    activities_timeline = activities.timeline_query(user.id,
+                                                    from_date,
+                                                    now,
+                                                    {'at_top'}).all()
+    return activities_timeline
+
 
 def process():
     config = get_config("notification.yaml")
@@ -34,20 +64,32 @@ def process():
             user_list = session.query(model.AppUser)\
                 .filter(model.AppUser.enabled)\
                 .all()
+
             if user_list is None:
                 break
+
             for user in user_list:
                 now = datetime.datetime.utcnow()
                 if user.email_time == None or user.email_interval == None:
-                    send_email(config, user)
-                    user.email_time = now
+                    activities = get_activities(session, user)
+                    if len(activities) != 0:
+                        send_email(config, user, activities)
+                        user.email_time = now
                 elif user.email_time + datetime.timedelta(seconds=user.email_interval) < now: 
-                    send_email(config, user)
-                    user.email_time = now
+                    activities = get_activities(session, user)
+                    if len(activities) != 0:
+                        send_email(config, user, activities)
+                        user.email_time = now
+
             session.commit()
 
             log.info("Job finished. Email sent for notification")
             time.sleep(interval)
+
+
+def connect_db():
+    model.connect_db(os.environ.get('DATABASE_URL'))
+
 
 if __name__ == "__main__":
     try:
