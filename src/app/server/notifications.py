8dee29eea510c4f97f5b506e9d3de2ace6e6e1c8
Alex Fraser <alex@phatcore.com>
2015-12-11 16:37:20 +1100

    Improved template, and improved user DB query
    - Changed notification email template to match the template used
      for the timeline in the web app
    - Added messages to the bottom of the email in case not all
      messages could be displayed
    - Using database time instead of server time to avoid missing
      messages.

--- src/app/server/notifications.py
+++ src/app/server/notifications.py
@@ -1,38 +1,84 @@
 import datetime
 from email.mime.text import MIMEText
 import logging
+from mail import send, get_config
 import os
 from string import Template
 import time
 
-import model
+from sqlalchemy import extract, func
+
 import activity
-from mail import send, get_config
+import model
+
 
 logging.basicConfig(format='%(asctime)s %(message)s')
 log = logging.getLogger('notifications')
 log.setLevel(logging.INFO)
 
-interval = 60 * 60 # seconds
 
-
-def mail_content(activities):
+def mail_content(config, activities):
     content = ''
+    template_bc = Template(config['MESSAGE_BROADCAST'])
+    template_std = Template(config['MESSAGE_STANDARD'])
     for act in activities:
-        if act.verbs != 'broadcast':
-            content += act.message + ' '
-            content += act.subject.name + ' '
-            content += 'this ' + act.ob_type + ' - '
-            content += str(act.created) + '\n'
+        if 'broadcast' in act.verbs:
+            template = template_bc
         else:
-            content += act.message + '\n'
+            template = template_std
+
+        content += template.substitute(
+            user_name=act.subject.name, message=act.message,
+            ob_type=ob_type(act), verbs=verbs(act),
+            date=str(act.created))
+        content += "\n"
+
     return content
 
 
-def send_email(config, user, activities):
+def ob_type(action):
+    if action.ob_type == 'qnode':
+        return 'survey category'
+    elif action.ob_type == 'rnode':
+        return 'submission category'
+    else:
+        return action.ob_type
 
+
+def verbs(action):
+    expr = ""
+    for i, verb in enumerate(action.verbs):
+        if i > 0 and i == len(action.verbs) - 1
+            expr += " and "
+        elif i > 0
+            expr += ", "
+
+        if verb == 'create':
+            verb = 'created'
+        elif verb == 'update':
+            verb = 'modified'
+        elif verb == 'state':
+            verb = 'changed the state of'
+        elif verb == 'delete':
+            verb = 'deleted'
+        elif verb == 'relation':
+            verb = '(re)linked'
+        elif verb == 'reorder_children':
+            verb = 'reordered the children of'
+
+        expr += verb;
+
+    return expr;
+
+
+def send_email(config, user, activities, messages):
     template = Template(config['MESSAGE_CONTENT'])
-    msg = MIMEText(template.substitute(message=mail_content(activities)), 'text/plain')
+    content = mail_content(config, activities)
+    message_str = "\n".join(messages)
+    msg = MIMEText(
+        template.substitute(
+            activities=content, messages=message_str, user_id=user.id),
+        'text/plain')
 
     msg['Subject'] = config['MESSAGE_SUBJECT']
     msg['From'] = config['MESSAGE_SEND_FROM']
@@ -41,44 +87,54 @@ def send_email(config, user, activities):
     send(config, msg)
 
 
-def get_activities(session, user):
-
-    now = datetime.datetime.utcnow()
+def get_activities(session, user, until_date, messages):
     activities = activity.Activities(session)
     from_date = user.email_time
-    if from_date == None:
-        from_date = now + datetime.timedelta(days=-7)
-    activities_timeline = activities.timeline_query(user.id,
-                                                    from_date,
-                                                    now,
-                                                    {'at_top'}).all()
+    earliest_from_date = until_date - datetime.timedelta(days=15)
+    if from_date is None or from_date < earliest_from_date:
+        messages.append(
+            "Requested start date was too early (%s).\n"
+            "Showing events from %s instead." % (
+                from_date, earliest_from_date))
+        from_date = earliest_from_date
+    activity_query = activities.timeline_query(
+        user.id, from_date, until_date, {'at_top'})
 
-    return activities_timeline
+    activity_query = activity_query.limit(MAX_ACTIVITIES)
+    activities = list(activity_query.all())
+    if len(activities) >= MAX_ACTIVITIES:
+        messages.append(
+            "Notification limit reached. There may be more activities; to see\n"
+            "them, log in to the web app.")
+
+    return activity_query.all()
 
 
 def process():
     config = get_config("notification.yaml")
     while True:
+        n_sent = 0
         with model.session_scope() as session:
-            user_list = session.query(model.AppUser)\
-                .filter(model.AppUser.enabled)\
-                .all()
+            interval = extract('epoch', func.now() - model.AppUser.email_time)
+            user_list = (session.query(model.AppUser, func.now())
+                .filter(model.AppUser.enabled,
+                        model.AppUser.email_interval != 0,
+                        (model.AppUser.email_time == None) |
+                        (interval > model.AppUser.email_interval))
+                .all())
 
-            if user_list is None:
-                break
+            for user, now in user_list:
+                messages = []
+                activities = get_activities(session, user, now, messages)
+                if len(activities) > 0:
+                    send_email(config, user, activities, messages)
+                user.email_time = now
+                # Commit after each email to avoid multiple emails in case a
+                # later iteration fails
+                session.commit()
 
-            for user in user_list:
-                now = datetime.datetime.utcnow()
-                if (user.email_time == None or user.email_interval == None) or (
-                    user.email_time + datetime.timedelta(seconds=user.email_interval) < now):
-                    activities = get_activities(session, user)
-                    if len(activities) != 0:
-                        send_email(config, user, activities)
-                        user.email_time = now
-            session.commit()
-
-            log.info("Job finished. Email sent for notification")
-            time.sleep(interval)
+        log.info("Job finished. %d notification emails sent.", n_sent)
+        time.sleep(config.JOB_INTERVAL_SECONDS)
 
 
 def connect_db():
@@ -87,7 +143,7 @@ def connect_db():
 
 if __name__ == "__main__":
     try:
-        log.info("Starting notification service...:%s", datetime.datetime.utcnow())
+        log.info("Starting notification service: %s", datetime.datetime.utcnow())
         connect_db()
         time.sleep(interval)
         process()
