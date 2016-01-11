82dcfd5f7da0b42b90754794602a1bf930ba197c
Alex Fraser <alex@phatcore.com>
2016-01-11 13:29:57 +1100

    Added new logo and icon.
    Also made the favicon expire when the app is deployed.

--- src/app/client/images/build-icons.sh
+++ src/app/client/images/build-icons.sh
@@ -5,13 +5,15 @@ imgdir=$(dirname $0)
 # High-res icons show full image
 for size in 72 96 128 144 192; do
     inkscape -z -w $size -h $size --export-area-page \
-        --export-png=${imgdir}/logo-monogram-$size.png ${imgdir}/logo-monogram.svg
+        --export-png=${imgdir}/logo-monogram-$size.png \
+        ${imgdir}/logo-monogram.svg
 done
 
 # Low-res icons show clipped image
 for size in 32 36 48 64; do
-    inkscape -z -w $size -h $size --export-area=25:60:155:190 \
-        --export-png=${imgdir}/logo-monogram-$size.png ${imgdir}/logo-monogram.svg
+    inkscape -z -w $size -h $size --export-area=25:48:167:190 \
+        --export-png=${imgdir}/logo-monogram-$size.png \
+        ${imgdir}/logo-monogram.svg
 done
 
 # Optimised SVG files
