e712a9bd296cc715832661b0ce2009e0303b41af
Alex Fraser <alex@phatcore.com>
2016-08-22 13:55:52 +1000

    Added missing index rename commands to upgrade script

--- src/app/alembic/versions/1ef306add5e_renamed_survey_to_program_and_hierarchy_.py
+++ src/app/alembic/versions/1ef306add5e_renamed_survey_to_program_and_hierarchy_.py
@@ -39,24 +39,36 @@ def upgrade():
 
     op.execute('ALTER TABLE measure RENAME COLUMN survey_id TO program_id')
     op.execute('ALTER TABLE measure RENAME CONSTRAINT measure_survey_id_fkey TO measure_program_id_fkey')
+
     op.execute('ALTER TABLE purchased_survey RENAME COLUMN survey_id TO program_id')
     op.execute('ALTER TABLE purchased_survey RENAME COLUMN hierarchy_id TO survey_id')
     op.execute('ALTER TABLE purchased_survey RENAME CONSTRAINT purchased_survey_survey_id_fkey TO purchased_survey_program_id_fkey')
     op.execute('ALTER TABLE purchased_survey RENAME CONSTRAINT purchased_survey_hierarchy_id_fkey TO purchased_survey_survey_id_fkey')
+
+    op.execute('ALTER INDEX qnodemeasure_measure_id_survey_id_index RENAME TO qnodemeasure_measure_id_program_id_index')
+    op.execute('ALTER INDEX qnodemeasure_qnode_id_survey_id_index RENAME TO qnodemeasure_qnode_id_program_id_index')
     op.execute('ALTER TABLE qnode_measure_link RENAME COLUMN survey_id TO program_id')
     op.execute('ALTER TABLE qnode_measure_link RENAME CONSTRAINT qnode_measure_link_survey_id_fkey TO qnode_measure_link_program_id_fkey')
+
+    op.execute('ALTER INDEX qnode_parent_id_survey_id_index RENAME TO qnode_parent_id_program_id_index')
+    op.execute('ALTER INDEX qnode_hierarchy_id_survey_id_index RENAME TO qnode_survey_id_program_id_index')
     op.execute('ALTER TABLE qnode RENAME COLUMN survey_id TO program_id')
     op.execute('ALTER TABLE qnode RENAME COLUMN hierarchy_id TO survey_id')
     op.execute('ALTER TABLE qnode RENAME CONSTRAINT qnode_survey_id_fkey TO qnode_program_id_fkey')
     op.execute('ALTER TABLE qnode RENAME CONSTRAINT qnode_hierarchy_id_fkey TO qnode_survey_id_fkey')
+
     op.execute('ALTER TABLE response_history RENAME COLUMN survey_id TO program_id')
     op.execute('ALTER TABLE response_history RENAME COLUMN assessment_id TO submission_id')
     op.execute('ALTER TABLE response_history RENAME CONSTRAINT response_history_survey_id_fkey TO response_history_program_id_fkey')
     op.execute('ALTER TABLE response_history RENAME CONSTRAINT response_history_assessment_id_fkey TO response_history_submission_id_fkey')
+
+    op.execute('ALTER INDEX response_assessment_id_measure_id_index RENAME TO response_submission_id_measure_id_index')
     op.execute('ALTER TABLE response RENAME COLUMN survey_id TO program_id')
     op.execute('ALTER TABLE response RENAME COLUMN assessment_id TO submission_id')
     op.execute('ALTER TABLE response RENAME CONSTRAINT response_survey_id_fkey TO response_program_id_fkey')
     op.execute('ALTER TABLE response RENAME CONSTRAINT response_assessment_id_fkey TO response_submission_id_fkey')
+
+    op.execute('ALTER INDEX rnode_qnode_id_assessment_id_index RENAME TO rnode_qnode_id_submission_id_index')
     op.execute('ALTER TABLE rnode RENAME COLUMN survey_id TO program_id')
     op.execute('ALTER TABLE rnode RENAME COLUMN assessment_id TO submission_id')
     op.execute('ALTER TABLE rnode RENAME CONSTRAINT rnode_survey_id_fkey TO rnode_program_id_fkey')
@@ -66,24 +78,36 @@ def upgrade():
 def downgrade():
     op.execute('ALTER TABLE measure RENAME COLUMN program_id TO survey_id')
     op.execute('ALTER TABLE measure RENAME CONSTRAINT measure_program_id_fkey TO measure_survey_id_fkey')
+
     op.execute('ALTER TABLE purchased_survey RENAME COLUMN survey_id TO hierarchy_id')
     op.execute('ALTER TABLE purchased_survey RENAME COLUMN program_id TO survey_id')
     op.execute('ALTER TABLE purchased_survey RENAME CONSTRAINT purchased_survey_survey_id_fkey TO purchased_survey_hierarchy_id_fkey')
     op.execute('ALTER TABLE purchased_survey RENAME CONSTRAINT purchased_survey_program_id_fkey TO purchased_survey_survey_id_fkey')
+
+    op.execute('ALTER INDEX qnodemeasure_measure_id_program_id_index RENAME TO qnodemeasure_measure_id_survey_id_index')
+    op.execute('ALTER INDEX qnodemeasure_qnode_id_program_id_index RENAME TO qnodemeasure_qnode_id_survey_id_index')
     op.execute('ALTER TABLE qnode_measure_link RENAME COLUMN program_id TO survey_id')
     op.execute('ALTER TABLE qnode_measure_link RENAME CONSTRAINT qnode_measure_link_program_id_fkey TO qnode_measure_link_survey_id_fkey')
+
+    op.execute('ALTER INDEX qnode_parent_id_program_id_index RENAME TO qnode_parent_id_survey_id_index')
+    op.execute('ALTER INDEX qnode_survey_id_program_id_index RENAME TO qnode_hierarchy_id_survey_id_index')
     op.execute('ALTER TABLE qnode RENAME COLUMN survey_id TO hierarchy_id')
     op.execute('ALTER TABLE qnode RENAME COLUMN program_id TO survey_id')
     op.execute('ALTER TABLE qnode RENAME CONSTRAINT qnode_survey_id_fkey TO qnode_hierarchy_id_fkey')
     op.execute('ALTER TABLE qnode RENAME CONSTRAINT qnode_program_id_fkey TO qnode_survey_id_fkey')
+
     op.execute('ALTER TABLE response_history RENAME COLUMN submission_id TO assessment_id')
     op.execute('ALTER TABLE response_history RENAME COLUMN program_id TO survey_id')
     op.execute('ALTER TABLE response_history RENAME CONSTRAINT response_history_submission_id_fkey TO response_history_assessment_id_fkey')
     op.execute('ALTER TABLE response_history RENAME CONSTRAINT response_history_program_id_fkey TO response_history_survey_id_fkey')
+
+    op.execute('ALTER INDEX response_submission_id_measure_id_index RENAME TO response_assessment_id_measure_id_index')
     op.execute('ALTER TABLE response RENAME COLUMN submission_id TO assessment_id')
     op.execute('ALTER TABLE response RENAME COLUMN program_id TO survey_id')
     op.execute('ALTER TABLE response RENAME CONSTRAINT response_submission_id_fkey TO response_assessment_id_fkey')
     op.execute('ALTER TABLE response RENAME CONSTRAINT response_program_id_fkey TO response_survey_id_fkey')
+
+    op.execute('ALTER INDEX rnode_qnode_id_submission_id_index RENAME TO rnode_qnode_id_assessment_id_index')
     op.execute('ALTER TABLE rnode RENAME COLUMN submission_id TO assessment_id')
     op.execute('ALTER TABLE rnode RENAME COLUMN program_id TO survey_id')
     op.execute('ALTER TABLE rnode RENAME CONSTRAINT rnode_submission_id_fkey TO rnode_assessment_id_fkey')
