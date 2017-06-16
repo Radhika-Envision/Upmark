31af279bdd37b2b3eac52ccb509d4dd7e79903ff
Alex Fraser <alex@phatcore.com>
2017-06-16 11:29:45 +1000

    Displaying modified time and version on query page
    And improved date/time display on other pages

--- src/app/client/js/widgets.js
+++ src/app/client/js/widgets.js
@@ -961,6 +961,41 @@ angular.module('vpac.widgets', [])
 })
 
 
+.directive('printFriendlyTimeago', function() {
+    return {
+        restrict: 'A',
+        scope: {
+            date: '=printFriendlyTimeago',
+            format: '@?pfFormat',
+            titleFormat: '@?pfTitleFormat',
+            units: '@?pfUnits',
+        },
+        template: '<span class="hidden-print" '+
+                        'title="{{date * multiplier | date:titleFormat}}">' +
+                    '{{date * multiplier | timeAgo:format}}</span>' +
+                  '<span class="visible-print-inline">' +
+                    '{{date * multiplier | date:format}}</span>',
+        link: function (scope, elem, attrs) {
+            if (!scope.format)
+                scope.format = 'mediumDate';
+            if (!scope.titleFormat)
+                scope.titleFormat = 'medium';
+            if (!scope.units)
+                scope.units = 'ms';
+            scope.multiplier = 1.0;
+            scope.$watch('units', function(units) {
+                scope.multiplier = {
+                    s: 1,
+                    ms: 1000,
+                    us: 1000000,
+                    μs: 1000000,
+                }[units];
+            });
+        }
+    };
+})
+
+
 .filter('markdown', function() {
     var converter = new showdown.Converter({
         strikethrough: true,
