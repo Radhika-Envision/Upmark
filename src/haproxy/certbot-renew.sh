ef98320302eaaf6dcf06b7c9ccc187d442dfddcb
Alex Fraser <alex@phatcore.com>
2017-07-05 14:22:13 +1000

    Added logging and basic error handling to certbot (Let's Encrypt)
    The Let's Encrypt renew script has been upgraded:
     - Turns on swap space to prevent OOM errors on low-memory servers
     - Reports failure to syslog

--- src/haproxy/certbot-renew.sh
+++ src/haproxy/certbot-renew.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
-service haproxy reload
+logger -p user.notice -t certbot "Attempting renewal"
+
+# certbot-auto uses pip to build stuff, which can cause the machine to run out
+# of memory. So enable a swap file first - but disable afterwards, since it
+# can hurt the SSD hard drive.
+trap 'swapoff /certbot-swapfile' EXIT
+if [ ! -e /certbot-swapfile ]; then
+    logger -p user.notice -t certbot "Creating swap space"
+    fallocate -l 2G /certbot-swapfile
+    chmod 600 /certbot-swapfile
+    mkswap /certbot-swapfile
+fi
+swapon /certbot-swapfile
 
 /usr/local/bin/certbot-auto renew \
     --standalone \
-    --standalone-supported-challenges http-01 \
+    --preferred-challenges http-01 \
     --http-01-port 54321 \
     --quiet \
-    --no-self-upgrade
+    --no-self-upgrade \
+    --no-bootstrap
+
+if [ $? -ne 0 ]; then
+    logger -p user.err -t certbot \
+        "Certbot failed. Run again manually to see error."
+    exit 1
+fi
 
 mkdir -p /etc/haproxy/certs
 
